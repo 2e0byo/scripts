4ab4a500d40e20538b8c6eecc83463950dcbd589
John Maximilian <2e0byo@gmail.com>
2023-08-11 16:42:04 +0200

    feat: class to handle stateful system properties.

---
+++ staterestore.py
@@ -0,0 +1,45 @@
+import json
+from abc import ABC, abstractmethod
+from contextlib import AbstractContextManager
+from pathlib import Path
+from traceback import print_exception
+
+
+class Logexception(AbstractContextManager):
+    def __exit__(
+        self,
+        *args,
+    ) -> bool | None:
+        _, value, _ = args
+        if value:
+            print_exception(value)
+        return True
+
+
+class Stateful(ABC):
+    name: str
+    sanitised_state: dict
+
+    def __init__(self, cachedir: Path = Path("/tmp/")):
+        self.cachef = cachedir / f"{self.name}.json"
+
+    @abstractmethod
+    def get_state(self) -> dict:
+        """Get current state"""
+
+    @abstractmethod
+    def set_state(self, state: dict):
+        """Get given state"""
+
+    def sanitise(self):
+        print("Sanitising", self.name)
+        with Logexception():
+            if not self.cachef.exists():
+                self.cachef.write_text(json.dumps(self.get_state()))
+            self.set_state(self.sanitised_state)
+
+    def restore(self):
+        print("Restoring", self.name)
+        with Logexception():
+            self.set_state(json.loads(self.cachef.read_text()))
+            self.cachef.unlink()
